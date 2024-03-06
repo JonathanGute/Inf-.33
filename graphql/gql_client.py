a26f6a7f44f106b81712aa6db72d82d384234bc2
Jonathan Gerson <73269233+JonathanGute@users.noreply.github.com>
2024-03-06 17:52:54 +0000

    mas cambios

---
+++ graphql/gql_client.py
@@ -0,0 +1,18 @@
+import requests
+
+# Definir la consulta GraphQL
+query = """
+    {
+        estudiante(id: 1, nombre: "Jose", apellido:"Lopez"){
+            nombre
+            apellido
+        }
+    }
+"""
+
+# Definir la URL del servidor GraphQL
+url = 'http://localhost:8000/graphql'
+
+# Solicitud POST al servidor GraphQL
+response = requests.post(url, json={'query': query})
+print(response.text)
